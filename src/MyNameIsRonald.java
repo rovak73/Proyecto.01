6679b10c81cd9bc3c0d536d03fef636d62022563
Ronald van Klaveren <rovak73@gmail.com>
2020-06-15 16:14:57 -0400

    First Commit

---
+++ src/MyNameIsRonald.java
@@ -0,0 +1,14 @@
+/*
+Primer programa de Java escrito por rovak73@gmail.com
+ */
+public class MyNameIsRonald {
+    public static void main (String[] args) {
+        System.out.println("Mi nombre es Ronald");//Este es mi nombre
+        System.out.println("Y mi apellido es van Klaveren");//Este es mi apellido
+        System.out.println("Y mi dirección es:\nFitz Roy 1431\nSantiago de Chile");//Esta es mi direccion
+        System.out.println();
+        System.out.print("y esta ");
+        System.out.print("es el primer ");
+        System.out.print("programa en Java");
+    }
+}
